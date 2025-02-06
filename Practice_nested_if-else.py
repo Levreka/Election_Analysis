cb43835866cc6c31d54ad7f4e6c3659173e5ea22
Franklin Tello <franklintello@msn.com>
2025-02-06 12:18:57 -0700

    updating comments and practice code

--- Practice_nested_if-else.py
+++ Practice_nested_if-else.py
@@ -1,7 +1,30 @@
-# What is the score?
+#this is to determine the letter grade demostrated 
+#using else if
+#What is the score?
 score = int(input("What is your test score? "))
 
 # Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+else:
+    if score >= 80:
+        print('Your grade is a B.')
+    else:
+        if score >= 70:
+            print('Your grade is a C.')
+        else:
+            if score >= 60:
+                print('Your grade is a D.')
+            else:
+                print('Your grade is an F.')
+                
+
+# What is the score? sam principal as before but
+# with if-elif-else statement
+score = int(input("What is your test score? "))
+
+# Determine the grade. notice important is the
+# indentation 
 if score >= 90:
     print('Your grade is an A.')
 elif score >= 80:
@@ -13,10 +36,3 @@ elif score >= 60:
 else:
     print('Your grade is an F.')
 
-candidate_votes = int(input("How many votes did the candidate get in the election? "))
-total_votes = int(input("What is the total number of votes in the election? "))
-message_to_candidate =(
-    f"You received {candidate_votes} number of votes."
-    f"The total number of votes in the election was {total_votes}."
-    f"You received {candidate_votes / total_votes * 100}% of the total votes.")
-print(message_to_candidate)
